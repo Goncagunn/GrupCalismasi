359bc0d3301183443ea8442cbc36c80d73e2411f
unknown <heratan2501@gmail.com>
2020-11-26 15:10:37 +0300

    ikinci mesaj

--- src/git/Grup.java
+++ src/git/Grup.java
@@ -5,6 +5,7 @@ public class Grup {
 	public static void main(String[] args) {
 		System.out.println("13:52 - ilk commit");
 		System.out.println("15:00 -ikinci commit");
+		System.out.println("15:07 - ucuncu commit");
 	}
 
 }
